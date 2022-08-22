71594340243c330b7b14b17830c61b6064cd7d48
riadshalahinleon <riadshalahinleon@gmail.com>
2022-08-23 00:27:39 +0600

    budget js part done

---
+++ js/budget.js
@@ -0,0 +1,36 @@
+function calculateTotalExpanse(elementId){
+    const budgetField  = document.getElementById(elementId);
+    const BudgetString = budgetField.value;
+    const Budget = parseInt(BudgetString);
+    return Budget;
+    
+}
+
+function totalPlayerExpanse(){
+    const  Budget =  calculateTotalExpanse('per-player-field')
+    const playerExpanseField = document.getElementById('expanse-field');
+    const totalPlayerBudget = Budget * 5 ;
+    playerExpanseField.innerText = totalPlayerBudget;
+    return totalPlayerBudget;
+}
+
+
+
+
+document.getElementById('btn-per-player').addEventListener('click' , function(){
+  
+    totalPlayerExpanse();
+
+    
+})
+
+document.getElementById('btn-total-calc').addEventListener('click' , function(){
+    const managerExpanse = calculateTotalExpanse('manager-field');
+    const coachExpanse = calculateTotalExpanse('coach-field');
+
+    const totatExpanse = managerExpanse + coachExpanse + totalPlayerExpanse() ;
+   
+    const totalField  = document.getElementById('total-amount');
+    totalField.innerText = totatExpanse;
+    
+})
