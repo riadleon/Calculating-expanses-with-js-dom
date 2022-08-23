723e3b61c56eb34ecff50f8f17ba974f18b2d86e
riadshalahinleon <riadshalahinleon@gmail.com>
2022-08-23 13:36:06 +0600

    card js done without msg

--- js/budget.js
+++ js/budget.js
@@ -1,15 +1,17 @@
-function calculateTotalExpanse(elementId){
-    const budgetField  = document.getElementById(elementId);
+function calculateTotalExpanse(elementId) {
+    const budgetField = document.getElementById(elementId);
     const BudgetString = budgetField.value;
     const Budget = parseInt(BudgetString);
     return Budget;
-    
+
 }
 
-function totalPlayerExpanse(){
-    const  Budget =  calculateTotalExpanse('per-player-field')
+function totalPlayerExpanse() {
+
+    const orderList = document.getElementById('orderd-list');
+    const Budget = calculateTotalExpanse('per-player-field')
     const playerExpanseField = document.getElementById('expanse-field');
-    const totalPlayerBudget = Budget * 5 ;
+    const totalPlayerBudget = Budget * orderList.children.length;
     playerExpanseField.innerText = totalPlayerBudget;
     return totalPlayerBudget;
 }
@@ -17,20 +19,20 @@ function totalPlayerExpanse(){
 
 
 
-document.getElementById('btn-per-player').addEventListener('click' , function(){
-  
+document.getElementById('btn-per-player').addEventListener('click', function () {
+
     totalPlayerExpanse();
 
-    
+
 })
 
-document.getElementById('btn-total-calc').addEventListener('click' , function(){
+document.getElementById('btn-total-calc').addEventListener('click', function () {
     const managerExpanse = calculateTotalExpanse('manager-field');
     const coachExpanse = calculateTotalExpanse('coach-field');
 
-    const totatExpanse = managerExpanse + coachExpanse + totalPlayerExpanse() ;
-   
-    const totalField  = document.getElementById('total-amount');
+    const totatExpanse = managerExpanse + coachExpanse + totalPlayerExpanse();
+
+    const totalField = document.getElementById('total-amount');
     totalField.innerText = totatExpanse;
-    
+
 })
--- js/card.js
+++ js/card.js
@@ -1,39 +1,11 @@
-function addPlayer(elementId , liId) {
-    const player = document.getElementById(elementId);
-    const playerString = player.innerText;
-    const bestFiveOneField = document.getElementById(liId);
-    bestFiveOneField.innerText = playerString;
+
+function playerSelect(event, btnId) {
+    const orderList = document.getElementById('orderd-list');
+    const newItem = document.createElement('li');
+    const itemValue = event;
+    newItem.innerText = itemValue;
+    orderList.appendChild(newItem);
+    document.getElementById(btnId).disabled = true;
 }
 
 
-
-document.getElementById('messi-btn').addEventListener('click', function () {
-
-    addPlayer('messi-txt' , 'li-one');
-   
-})
-document.getElementById('ney-btn').addEventListener('click', function () {
-
-    addPlayer('ney-txt' , 'li-two');
-   
-})
-document.getElementById('bappe-btn').addEventListener('click', function () {
-
-    addPlayer('bappe-txt' , 'li-three');
-   
-})
-document.getElementById('paredes-btn').addEventListener('click', function () {
-
-    addPlayer('paredes-txt' , 'li-four');
-   
-})
-document.getElementById('renato-btn').addEventListener('click', function () {
-
-    addPlayer('renato-txt' , 'li-five');
-   
-})
-document.getElementById('dona-btn').addEventListener('click', function () {
-
-    addPlayer('dona-txt'  );
-   
-})
