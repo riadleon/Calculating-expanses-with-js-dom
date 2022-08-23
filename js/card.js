002bcd5befb968d48e5a53530ee620f13167b545
riadshalahinleon <riadshalahinleon@gmail.com>
2022-08-23 14:38:19 +0600

    card js finally done with msg

--- js/card.js
+++ js/card.js
@@ -1,11 +1,18 @@
 
 function playerSelect(event, btnId) {
     const orderList = document.getElementById('orderd-list');
-    const newItem = document.createElement('li');
-    const itemValue = event;
-    newItem.innerText = itemValue;
-    orderList.appendChild(newItem);
-    document.getElementById(btnId).disabled = true;
+    if (orderList.children.length != 5) {
+        const newItem = document.createElement('li');
+        const itemValue = event;
+        newItem.innerText = itemValue;
+        orderList.appendChild(newItem);
+        document.getElementById(btnId).disabled = true;
+    }
+    else{
+        alert('Please Choto Bhai!! Keep Your Head Coool');
+    }
+
+
 }
 
 
